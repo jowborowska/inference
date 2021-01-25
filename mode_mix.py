8c9279276386f06100c4dd2d3255f8545f2e40e9
Jowita Borowska <54434838+jowborowska@users.noreply.github.com>
2021-01-25 14:28:59 +0100

    Add files via upload

--- mode_mix.py
+++ mode_mix.py
@@ -126,13 +126,13 @@ ax1.errorbar(k_arr[0],our_estimate, error_bars, color='black')
 ax1.set_ylabel(r'$\mathrm{P^{weights}(k)/P(k)}$', fontsize=14)
 ax1.set_xlabel(r'$k$ [Mpc${}^{-1}$]', fontsize=14)
 labnums = [0.05,0.1, 0.2, 0.5]
-ax1.set_xlim(0.04,0.7)
+ax1.set_xlim(0.05,0.7)
 ax1.set_xscale('log')
-ax1.set_ylim(0.5,3.)
+ax1.set_ylim(0.5,1.5)
 ax1.grid()
 ax1.set_xticks(labnums)
 ax1.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
 plt.tight_layout()
-plt.savefig('mode_mix2.png')
+plt.savefig('mode_mix2.pdf')
 
 
